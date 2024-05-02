2e11219f5353f67bac500ff7a8ded755a86de7c4
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2024-05-02 15:39:45 +0530

    basic react router

--- responsive-app/src/App.js
+++ responsive-app/src/App.js
@@ -1,17 +1,29 @@
 import React from "react";
-import ResponsiveButton from "./components/ResponsiveButton";
-import FlexBox1 from "./components/FlexBox1";
-import AddTodo from "./components/AddTodo";
-import Todos from "./components/Todos";
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
 
   return (
-    <div style={{marginTop: '50px', textAlign:'center'}}>
-     <AddTodo/>
-     <Todos/> 
-    </div>
+    <BrowserRouter >
+    <header style={{padding:'20px'}}>
+      <nav>
+        <h1>Router</h1>
+        <Link to='/'> Home</Link>
+        <NavLink to="/about" style={{paddingLeft:'20px'}}>About</NavLink> 
+      </nav>
+    </header>
+    <main style={{padding:'20px'}}>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </main>
+    </BrowserRouter>
+    
   );
 }
+//we can add additional css to navlink but not for the normal link. use of navlink is recommended.
 
 export default App;
