cbf1c12e8df6c4056beb636b9f10963994afc828
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2024-05-16 16:01:49 +0530

    using Navigate component from react router

--- responsive-app/src/pages/About.js
+++ responsive-app/src/pages/About.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Navigate } from 'react-router-dom';
 
 export default function About() {
+  const [user, setUser] = useState('ramu');
+
+  if(!user){
+    return <Navigate to="/" replace={true}/>
+  }
+
   return (
-    <div>About page</div>
+
+    <div>About page 
+      <br/>
+
+      <button onClick={()=>setUser(null)}>Logout</button>
+
+    </div>
+    
   )
 }
