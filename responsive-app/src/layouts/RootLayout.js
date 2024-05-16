788b36b35814448605ca86be4ecd182a81d380a3
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2024-05-16 11:40:45 +0530

    making breadcrumbs

--- responsive-app/src/layouts/RootLayout.js
+++ responsive-app/src/layouts/RootLayout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import Breadcrumbs from '../components/Breadcrumbs';
 
 export default function RootLayout() {
   return (
@@ -12,6 +13,7 @@ export default function RootLayout() {
                 <NavLink to="/help" style={{paddingLeft:'20px'}}>Help</NavLink> 
                 <NavLink to="/careers" style={{paddingLeft:'20px'}}>Careers</NavLink> 
             </nav>
+            <Breadcrumbs/>
         </header>
         <main>
             <Outlet/>
